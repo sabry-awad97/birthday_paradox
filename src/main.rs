2b9fb8ffeeeaf2958c2e494b069fa35aada80af7
Sabry Awad <dr.sabry1997@gmail.com>
2023-09-28 00:16:53 +0300

    ✨ feat(main.rs): Implement Birthday Paradox Checker 📝 Summary: Implemented a Birthday Paradox Checker program in Rust. The program calculates the probability of at least two people sharing a birthday in a group of a given size after a specified number of trials. 💡 Details: - The program generates random groups of birthdays and checks for duplicate birthdays within each group. - It calculates the probability of finding at least one duplicate birthday in the generated groups. - User input is collected for the group size and the number of trials, with input validation. - Execution time is measured to provide performance information. - The results, including the total matches found and the calculated probability, are displayed to the user. ➕ Dependencies: - Added dependency on the `rand` crate for generating random numbers. - Used the standard library (`std::collections::HashSet` and `std::time::Instant`) for other functionalities. 🔍 Context: - The program is designed to illustrate the Birthday Paradox, a famous probability problem. - It generates random groups of birthdays and checks for shared birthdays, repeating the process for a specified number of trials. - This Rust program helps users understand the concept through practical experimentation. 📚 Challenges: - N/A ✍️ Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,98 @@
+use rand::Rng;
+use std::collections::HashSet;
+use std::time::Instant;
+
+fn generate_random_birthday() -> String {
+    // Generate a random birthday in MM/DD format
+    let month = rand::thread_rng().gen_range(1..=12);
+    let day = rand::thread_rng().gen_range(1..=31);
+    format!("{:02}/{:02}", month, day)
+}
+
+fn generate_random_group(size: usize) -> Vec<String> {
+    // Generate a group of random birthdays
+    (0..size).map(|_| generate_random_birthday()).collect()
+}
+
+fn check_for_duplicates(birthdays: &[String]) -> bool {
+    // Check if there are any duplicate birthdays in the group
+    let mut unique_birthdays = HashSet::new();
+    for birthday in birthdays.iter() {
+        if !unique_birthdays.insert(birthday.clone()) {
+            return true;
+        }
+    }
+    false
+}
+
 fn main() {
-    println!("Hello Rustacean!");
+    println!("Welcome to the Birthday Paradox Checker!");
+
+    // Ask the user for input
+    let group_size = get_group_size_from_user();
+    let num_trials = get_num_trials_from_user();
+
+    let start_time = Instant::now();
+    let mut total_matches = 0;
+
+    // Run multiple trials
+    for _ in 0..num_trials {
+        let birthdays = generate_random_group(group_size);
+        if check_for_duplicates(&birthdays) {
+            total_matches += 1;
+        }
+    }
+
+    let end_time = Instant::now(); // Stop measuring execution time
+    let elapsed_time = end_time.duration_since(start_time);
+
+    let probability = (total_matches as f64 / num_trials as f64) * 100.0;
+
+    // Display results
+    println!(
+        "\nResults after {} trials with a group size of {}:",
+        num_trials, group_size
+    );
+    println!("Total matches found: {}", total_matches);
+    println!(
+        "Probability of at least two people sharing a birthday: {:.2}%",
+        probability
+    );
+    println!("Elapsed time: {:?}", elapsed_time);
+}
+
+fn get_group_size_from_user() -> usize {
+    // Prompt the user for the group size and validate the input
+    loop {
+        println!("Enter the size of the group (at least 2): ");
+        let mut input = String::new();
+        std::io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+        match input.trim().parse() {
+            Ok(size) if size >= 2 => return size,
+            _ => {
+                println!("Please enter a valid group size (at least 2).");
+                continue;
+            }
+        };
+    }
+}
+
+fn get_num_trials_from_user() -> usize {
+    // Prompt the user for the number of trials and validate the input
+    loop {
+        println!("Enter the number of trials: ");
+        let mut input = String::new();
+        std::io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+        match input.trim().parse() {
+            Ok(trials) if trials > 0 => return trials,
+            _ => {
+                println!("Please enter a valid number of trials (greater than 0).");
+                continue;
+            }
+        };
+    }
 }
